2587fc869525ffba402f2c6b48b9bd0a69626996
Abiel Mainty <maintabi001@warren.k12.in.us>
2024-11-01 08:37:24 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+card1 = int(input("Enter the value for card1"))
+card2 = int(input("Enter the value for card2 "))
+
+# TODO: Make a variable called card_sum and
+Card_sum = card1 + card2
+is_21_or_over = Card_sum > 21
+# TODO: print the result
+print("card1 + card2 =card_sum > 21")
